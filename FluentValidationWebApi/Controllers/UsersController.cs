41a15f184bba4a3336b7b138bc974d581ad73791
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-11-01 13:13:40 +0600

    hasan

---
+++ FluentValidationWebApi/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidationWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] UserModel userModel)
+        {
+            // If validation fails, a 400 Bad Request will be returned automatically.
+            return CreatedAtAction(nameof(CreateUser), userModel);
+        }
+    }
+}
---
+++ FluentValidationWebApi/Handlers/UserModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidationWebApi.Models;
+
+namespace FluentValidationWebApi.Handlers
+{
+    public class UserModelValidator : AbstractValidator<UserModel>
+    {
+        public UserModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Age)
+                .InclusiveBetween(18, 35).WithMessage("Age must be between 18 and 35.");
+        }
+    }
+}
